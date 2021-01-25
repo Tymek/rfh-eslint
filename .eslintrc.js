66c060548ff4e7fadca527cd557c77faaeaa0e2e
Tymoteusz Czech <mail@tymek.cz>
2021-01-25 22:27:09 +0100

    eslint setup

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "extends": "react-app",
-  "parserOptions": {
-    "project": "./tsconfig.json",
-  },
-  "rules" : {
-    "@typescript-eslint/unbound-method": "error",
-  },
-}
--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <input ref={register()} name="username" />
+        <input ref={register()} name="username" data-testid="input" />
       </header>
     </div>
   );
