6a267a4cff30a5c89b2a2138f88b3e554fd922be
Tymoteusz Czech <mail@tymek.cz>
2021-01-25 22:11:48 +0100

    setup rhf

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": "react-app",
+  "parserOptions": {
+    "project": "./tsconfig.json",
+  },
+  "rules" : {
+    "@typescript-eslint/unbound-method": "error",
+  },
+}
